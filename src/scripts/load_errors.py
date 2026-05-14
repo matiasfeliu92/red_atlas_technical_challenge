cac937c10b393d6fc541efe0ab3656738fcd1a6e
matiasfeliu92 <matumazparrote@gmail.com>
2026-05-14 16:49:01 -0300

    implement load data in mongo db

--- src/scripts/load_errors.py
+++ src/scripts/load_errors.py
@@ -1,29 +1,24 @@
 from datetime import datetime, timezone
 import json
 
-from src.models.errors import Errors
 from src.config.logger import LoggerConfig
 from src.config.manage_db import ManageDB
 
 class LoadErrors:
     def __init__(self, db: ManageDB):
         self.db = db
+        self.collection = db.db["errors"]
         self.logger = LoggerConfig.get_logger(self.__class__.__name__)
 
     def insert_error(self, error_message):
         """Insert an error message into the database."""
-        session = self.db.Session()
         try:
-            error = Errors(
-                timestamp=datetime.now(timezone.utc).isoformat(),
-                error=json.dumps(error_message)
-            )
-            session.add(error)
-            session.commit()
+            doc = {
+                "timestamp": datetime.now(timezone.utc).isoformat(),
+                "error": json.dumps(error_message),
+            }
+            self.collection.insert_one(doc)
             self.logger.info(f"Error registrado en la base de datos: {error_message}")
         except Exception as e:
-            session.rollback()
             self.logger.error(f"Error inserting error message: {e}")
             raise
-        finally:
-            session.close()

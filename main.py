3f96641f705d5d2586a03304d39cde622a84abf3
matiasfeliu92 <matumazparrote@gmail.com>
2026-05-14 16:50:46 -0300

    add range iterator for scrap data using pagination path

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ if __name__ == "__main__":
     web_paths = settings.web_paths
     scraping = Scraping()
     for path in web_paths:
-        print(f"INICIANDO SCRAPING PARA: {path}")
-        scraped_data = scraping.scrap(path["path"], path["filter"])
-        load_data.insert_listings(scraped_data)
+        print(f"""INICIANDO SCRAPING PARA: {path["path"]}""")
+        for page_number in range(1,8):
+            url = path["path"] + f"/{page_number}_p"
+            scraped_data = scraping.scrap(url, path["filter"])
+            load_data.insert_listings(scraped_data)
